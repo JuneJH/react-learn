03a1b091bbcf94d81da6e1e5b8116441d37c2b9f
JuneJH <15310487388@163.com>
2021-01-07 11:42:10 +0800

    jsx

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const reactDom = React.createElement("div",{className:"div"},"",React.createElement("p",{style:{color:"yellow"}},"this is p element"),React.createElement("p",{},"this is span element"))
+
+const jsxDom = <div className="div"><p style={{color:"red"}}>this is p element</p><span>this is span element</span></div>
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   {reactDom}
   </React.StrictMode>,
   document.getElementById('root')
 );
