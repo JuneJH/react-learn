50ee4b4241454c7016b61028279eb61be1cd618f
JuneJH <15310487388@163.com>
2021-01-23 12:04:36 +0800

    portals

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,7 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function ChildA() {
+  return (<div>
+    <h1>=======ChildA===========</h1>
+    <p>childaaaaaaaaa</p>
+    <ChildB />
+  </div>)
+}
+
+function ChildB() {
+  return ReactDOM.createPortal((<div>
+    <h1>=======ChildB=======</h1>
+    <p>childbbbbbbbbb</p>
+    <ChildC />
+  </div>), document.getElementById("portals"))
+}
+
+function ChildC() {
+  return (<div>
+    <h1>ChildC</h1>
+  </div>)
+}
+
+
 
 function App() {
-  return (<div>
-    hello!!
+  return (<div onClick={()=>{console.log("点击了APP")}}>
+    <ChildA />
   </div>);
 }
 
