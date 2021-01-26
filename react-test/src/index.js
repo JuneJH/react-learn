f5c283af46508cedff73706d04d11fcc906b3225
JuneJH <15310487388@163.com>
2021-01-26 17:37:00 +0800

    渲染过程

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
+const app = (<div style={{ color: "red" }} className="app">
+  <p>我是p元素</p>
+  <h1>我是和h1元素</h1>
+</div>)
 ReactDOM.render(
-  <React.StrictMode>
-     <App/>
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
+
+console.log(app)
+
 
