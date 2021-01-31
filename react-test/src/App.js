9c227b08810579974f29a11915210f0811e4d9d1
JuneJH <15310487388@163.com>
2021-01-31 11:34:57 +0800

    事件处理

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,8 +1,18 @@
-
+let event = null;
 function App() {
-  return (<div>
-    hello!!
+  return (<div onClick={e => {
+    console.log("内置React组件div点击事件触发")
+    console.log("react事件对象是否重用",e === event)
+  }}>
+    <button onClick={(e) => {
+      console.log("内置组件button点击事件触发")
+      console.log("真实事件对象",e.nativeEvent)
+     event = e;
+    }}>
+      点击事件
+          </button>
   </div>);
 }
+
 
 export default App;
--- react-test/src/index.js
+++ react-test/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
+const container = document.getElementById("root");
+document.addEventListener("click", e => {
+  console.log("document事件:点击了");
+  e.stopPropagation();
+})
+container.onclick = e => {
+  console.log("真实DOM根元素点击事件触发");
+}
 ReactDOM.render(
   <React.StrictMode>
-     <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  container
 );
+
+
 
