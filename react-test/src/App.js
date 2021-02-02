1e9d7ac0b433fe4de1ad98d471f9314c120e4f23
JuneJH <15310487388@163.com>
2021-02-02 11:06:59 +0800

    setState

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,8 +1,29 @@
 
-function App() {
-  return (<div>
-    hello!!
-  </div>);
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    n:0,
+  }
+  componentDidMount(){
+    this.setState({n:this.state.n + 1});
+    console.log("1",this.state.n);
+    this.setState({n:this.state.n + 1});
+    console.log("2",this.state.n);
+
+    setTimeout(()=>{
+      this.setState({n:this.state.n + 1});
+      console.log("3",this.state.n)
+      this.setState({n:this.state.n + 1});
+      console.log("4",this.state.n)
+    },1000)
+  }
+  render() {
+    return (
+      <div>
+        {this.state.n}
+      </div>
+    )
+  }
 }
 
-export default App;
