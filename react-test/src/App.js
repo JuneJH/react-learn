b9dba4aa05fb22f42d02be3dbe5621bd6d98dd6a
JuneJH <15310487388@163.com>
2021-02-02 11:26:14 +0800

    setState

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -6,16 +6,20 @@ export default class App extends Component {
     n:0,
   }
   componentDidMount(){
-    this.setState({n:this.state.n + 1});
-    console.log("1",this.state.n);
-    this.setState({n:this.state.n + 1});
-    console.log("2",this.state.n);
+    this.setState({n:this.state.n + 1},()=>{
+      console.log("第一次调用setState")
+    });
+    console.log("1",this.state.n);    // 0
+    this.setState({n:this.state.n + 1},()=>{
+      console.log("第二次调用setState")
+    });
+    console.log("2",this.state.n);    // 0 
 
     setTimeout(()=>{
       this.setState({n:this.state.n + 1});
-      console.log("3",this.state.n)
+      console.log("3",this.state.n)   // 2
       this.setState({n:this.state.n + 1});
-      console.log("4",this.state.n)
+      console.log("4",this.state.n)    // 3
     },1000)
   }
   render() {
