640abd026d30defecc24e4410fc0ebf9cecaaf55
JuneJH <15310487388@163.com>
2021-02-23 16:58:30 +0800

    路由

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,33 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import qs from 'query-string'
 
-import React, { Component } from 'react'
-
-export default class App extends Component {
-  state = {
-    n:0,
-  }
-  componentDidMount(){
-    this.setState({n:this.state.n + 1},()=>{
-      console.log("第一次调用setState")
-    });
-    console.log("1",this.state.n);    // 0
-    this.setState({n:this.state.n + 1},()=>{
-      console.log("第二次调用setState")
-    });
-    console.log("2",this.state.n);    // 0 
-
-    setTimeout(()=>{
-      this.setState({n:this.state.n + 1});
-      console.log("3",this.state.n)   // 2
-      this.setState({n:this.state.n + 1});
-      console.log("4",this.state.n)    // 3
-    },1000)
-  }
-  render() {
-    return (
-      <div>
-        {this.state.n}
-      </div>
-    )
-  }
+function A(props) {
+  console.log(props);
+  return (<div>
+    <h1>A页面</h1>
+    <button onClick={() => {
+      props.history.push("/b?a=1&b=2&c=3#d=4","状态数据")
+    }}>跳转B页面</button>
+  </div>)
 }
 
+function B(props) {
+  console.log(props);
+  const params = qs.parse(props.location.search);
+  console.log(params)
+  return (<div>
+    <h1>B页面</h1>
+    <button onClick={() => {
+      props.history.goBack();
+    }}>跳转A页面</button>
+  </div>)
+}
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Link to="/a">A页面</Link>
+      <Route path="/a" component={A} />
+      <Route path="/b" component={B} />
+    </BrowserRouter>
+  )
+}
