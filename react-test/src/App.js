94cac917aef2874f9d50819ee514ee23ed66f31e
JuneJH <15310487388@163.com>
2021-02-24 10:07:09 +0800

    路由

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import qs from 'query-string'
+import { BrowserRouter, Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import qs from 'query-string';
+import "./app.css"
 
 function A(props) {
   console.log(props);
   return (<div>
     <h1>A页面</h1>
     <button onClick={() => {
-      props.history.push("/b?a=1&b=2&c=3#d=4","状态数据")
+      props.history.push("/b?a=1&b=2&c=3#d=4", "状态数据")
     }}>跳转B页面</button>
   </div>)
 }
@@ -19,16 +20,34 @@ function B(props) {
   return (<div>
     <h1>B页面</h1>
     <button onClick={() => {
-      props.history.goBack();
-    }}>跳转A页面</button>
+      props.history.push("/c");
+    }}>跳转C页面</button>
   </div>)
 }
+
+function C() {
+  return <div>
+    <Link to="/a">跳转A页面</Link>
+  </div>
+}
+
+// function MyLink(props){
+//   return <a href={props.to} onClick={e=>{
+//     e.preventDefault();
+//     props.history.push(props.to);
+//   }}>{props.children}</a>
+// }
+// const MyLinkW = withRouter(MyLink)
 export default function App() {
   return (
     <BrowserRouter>
-      <Link to="/a">A页面</Link>
-      <Route path="/a" component={A} />
-      <Route path="/b" component={B} />
+      <NavLink to="/a">A页面</NavLink>
+      <Switch>
+        {/* <Route path="/a" component={A} /> */}
+        <Route path="/b" component={B} />
+        <Route path="/c" component={C} />
+        <Redirect from="/a" to="/b">重定向</Redirect>
+      </Switch>
     </BrowserRouter>
   )
 }
